bf6549f2c692e8cf2b6a417131bd995151da322a
binh <binh@gmail.com>
2025-11-14 00:59:27 +0700

    Cap nhat trang admin

--- demo/src/main/java/com/example/demo/ConTroller/AdminController.java
+++ demo/src/main/java/com/example/demo/ConTroller/AdminController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
-
+    @GetMapping("/Dashboard")
+    public String admin() {
+        return "admin";
+    }
     @GetMapping("/admin/product")
     public String productList() {
         return "admin/product/products-list";
@@ -14,5 +17,13 @@ public class AdminController {
     @GetMapping("/admin/category")
     public String categoryList() {
         return "admin/category/category-list";
+    }
+    @GetMapping("/admin/Users")
+    public String users() {
+        return "admin/users/users-list";
+    }
+    @GetMapping("/admin/oders")
+    public String orders() {
+        return "admin/oders/orders-list";
     }
 }
